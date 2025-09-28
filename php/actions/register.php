ad9997b2f2af04fa326731b3823e777f6c884123
asadmukhtarr <axad03213@gmail.com>
2025-09-28 20:21:09 +0500

    database connection

--- php/actions/register.php
+++ php/actions/register.php
@@ -3,5 +3,6 @@
     $email     = $_POST['email'];
     $password  = $_POST['password'];
     $cpassword = $_POST['confirm_password'];
-    echo $cpassword;
+    // defualt user name of localhost database is "root" and there is no password ..
+    $cn = mysqli_connect('localhost','root','','loginsystemfswd') or die('cant connect database');
 ?>
